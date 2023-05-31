8140562b342c559ca92a3da8bc96c1b04b3e86df
SkimoBen <pearmanbenjamin@gmail.com>
2023-05-31 09:58:41 -0600

    Initial Commit

---
+++ Ultrathin Loading Text/Ultrathin_Loading_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ultrathin_Loading_TextApp.swift
+//  Ultrathin Loading Text
+//
+//  Created by Ben Pearman on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Ultrathin_Loading_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
